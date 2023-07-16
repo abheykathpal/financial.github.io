d408f34418a703154dc04866108624bcea8fe0ef
abheykathpal <abheyarora.ynr@gmail.com>
2023-07-16 22:48:55 +0530

    financial services

---
+++ style.js
@@ -0,0 +1,59 @@
+if (window.screen.width < 600) {
+    
+    function openmenu() {
+        document.getElementById("menu").style.display = "block";
+        document.getElementById("openmenu").style.display = "none";
+        document.getElementById("closemenu").style.display = "block";
+        document.getElementById("closemenu").style.marginTop = "-1.5em";
+    }
+
+
+    function closemenu() {
+        document.getElementById("menu").style.display = "none";
+        document.getElementById("openmenu").style.display = "block";
+        document.getElementById("closemenu").style.background = "none";
+    }
+
+    function fun1() {
+        document.getElementById('menu').style.display = "none";
+        document.getElementById("openmenu").style.display = "block";
+        document.getElementById("closemenu").style.display = "none";
+        document.getElementById("openmenu").style.marginTop = "-2.3em";
+    }
+
+    function fun2() {
+        document.getElementById('menu').style.display = "none";
+        document.getElementById("openmenu").style.display = "block";
+        document.getElementById("closemenu").style.display = "none";
+        document.getElementById("openmenu").style.marginTop = "-2.3em";
+    }
+
+    function fun3() {
+        document.getElementById('menu').style.display = "none";
+        document.getElementById("openmenu").style.display = "block";
+        document.getElementById("closemenu").style.display = "none";
+        document.getElementById("openmenu").style.marginTop = "-2.3em";
+    }
+
+    function fun4() {
+        document.getElementById('menu').style.display = "none";
+        document.getElementById("openmenu").style.display = "block";
+        document.getElementById("closemenu").style.display = "none";
+        document.getElementById("openmenu").style.marginTop = "-2.3em";
+    }
+
+    function fun5() {
+        document.getElementById('menu').style.display = "none";
+        document.getElementById("openmenu").style.display = "block";
+        document.getElementById("closemenu").style.display = "none";
+        document.getElementById("openmenu").style.marginTop = "-2.3em";
+    }
+
+    function fun6() {
+        document.getElementById('menu').style.display = "none";
+        document.getElementById("openmenu").style.display = "block";
+        document.getElementById("closemenu").style.display = "none";
+        document.getElementById("openmenu").style.marginTop = "-2.3em";
+    }
+
+}
